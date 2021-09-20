e71089fa3b2ed76bd5920c023f40deaa3d2d7fb4
SBOne-Kenobi <sbonekenobi@gmail.com>
2021-09-20 12:11:04 +0300

    test gpu

--- src/Program.fs
+++ src/Program.fs
@@ -1,13 +1,37 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+module MatrixMultiply
 
 open System
+open OpenCL.Net
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let random = Random()
 
-[<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let GenerateMatrix rows cols =
+    Array.init (rows * cols) (fun _ -> float32 <| random.NextDouble())
+
+let GetResultMatrixDims aRows aCols bRows bCols =
+    if aCols <> bRows then
+        failwith "Dimensions not match for multiplying"
+    aRows, bCols
+
+let Multiply (a:array<_>) aRows aCols (b:array<_>) bRows bCols (c:array<_>) =
+    let cRows, cCols = GetResultMatrixDims aRows aCols bRows bCols
+    for i in 0..cRows - 1 do
+        for j in 0..cCols - 1 do
+            let mutable sum = 0.f
+            for k in 0..aCols - 1 do
+                sum <- sum + a.[i * aCols + k] * b.[k * bCols + j]
+            c.[i * cCols + j] <- sum
+
+let GetDeviceName (device: Device) =
+    let mutable error = ErrorCode()
+    Cl.GetDeviceInfo(device, DeviceInfo.Name, &error).ToString()
+
+let GetAllDevices =
+    let mutable error = ErrorCode()
+    seq {
+        for platform in Cl.GetPlatformIDs(&error) do
+            yield! Cl.GetDeviceIDs(platform, DeviceType.All, &error)
+    }
+
+let Main =
+    printfn $"All devices: %A{GetAllDevices |> Seq.map GetDeviceName}"
